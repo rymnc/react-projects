9b93f53bd137b590bbacdefeaba62682d17f5717
aaryamannchallani <aaryamannchallani7@gmail.com>
2020-06-09 14:33:36 +0530

    Initial Commit fetch-my-api

---
+++ fetch-my-api/src/index.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import Axios from 'axios';
+
+const App = () =>{
+
+    const [call,setCall] = useState({
+        cToken:'',
+        address:'',
+        cTokenBalance:''
+    });
+
+    const handleClick = async () =>{
+        const result = Axios('https://salty-temple-30960.herokuapp.com/cTokenBalance/cDai/0x04636b11322756aBa94d09c1deAFdfa04D4f8A74')
+        console.log(result)
+    }
+
+    
+
+
+    return(
+        <div className='text-center'>
+            <button className="btn btn-primary" onClick={handleClick}>Get Data</button>
+        </div>
+    )
+}
+
+ReactDOM.render(<App/>,document.getElementById('root'))
