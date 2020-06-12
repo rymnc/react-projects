ceecff1243473b3702f6623f04f24e8bd8371c5e
aaryamannchallani <aaryamannchallani7@gmail.com>
2020-06-12 17:03:40 +0530

    Added giphy-loader

---
+++ giphy-loader/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Axios from 'axios';
+
+const App = () =>{
+    return(
+        <div className="container text-center mt-auto mb-auto p-3">
+            <h1 className="display-4">Hey</h1>
+        </div>
+    )
+};
+
+ReactDOM.render(<App/>,document.getElementById('root'))
